f7495bab177dc528df71529c6524d22df092651e
Awesomedonut <rainyjsong@gmail.com>
2025-08-26 16:41:12 -0700

    initial abp project setup

---
+++ src/MyAbpApp.EntityFrameworkCore/EntityFrameworkCore/MyAbpAppDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace MyAbpApp.EntityFrameworkCore;
+
+/* This class is needed for EF Core console commands
+ * (like Add-Migration and Update-Database commands) */
+public class MyAbpAppDbContextFactory : IDesignTimeDbContextFactory<MyAbpAppDbContext>
+{
+    public MyAbpAppDbContext CreateDbContext(string[] args)
+    {
+        var configuration = BuildConfiguration();
+        
+        MyAbpAppEfCoreEntityExtensionMappings.Configure();
+
+        var builder = new DbContextOptionsBuilder<MyAbpAppDbContext>()
+            .UseSqlite(configuration.GetConnectionString("Default"));
+        
+        return new MyAbpAppDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../MyAbpApp.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
